2d9e835358e81dc19181dc695a3b6f75297382e7
ruhim <ruhim@DESKTOP-43RLGVU>
2024-02-07 16:21:06 +0300

    Add project files.

---
+++ oopOdev/Entities/Instruction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopOdev.Entities
+{
+    public class Instruction
+    {
+        public Instruction()
+        {
+
+        }
+
+        public Instruction(int ıd, string name, string description, string ınstructorName, string price)
+        {
+            Id = ıd;
+            Name = name;
+            Description = description;
+            InstructorName = ınstructorName;
+            Price = price;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string InstructorName { get; set; }
+        public string Price { get; set; }
+        public Category Category { get; set; }
+    }
+}
